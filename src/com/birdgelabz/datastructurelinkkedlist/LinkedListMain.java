62c3bc25973b33712a2e041fa89938df795f13cf
NitinNb9393 <nitinbabar9393@gmail.com>
2021-11-08 12:54:50 +0530

    Added: Program for the adding the data at the between the other node

--- src/com/birdgelabz/datastructurelinkkedlist/LinkedListMain.java
+++ src/com/birdgelabz/datastructurelinkkedlist/LinkedListMain.java
@@ -4,18 +4,25 @@ import java.util.Scanner;
 
 public class LinkedListMain {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to the LinkedList Code !!");
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Press '1' to add the data at the start");
-		System.out.println("Press '2' to add the data at the end");
-		switch (scanner.nextInt()) {
-		case 1:
-			Operations.addDataAtStart();
-			break;
-		case 2:
-			Operations.addDataAtEnd();
-			break;
-		}
-	}
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the LinkedList Code !!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Press '1' to add the data at the start");
+        System.out.println("Press '2' to add the data at the end");
+        System.out.println("Press '3' to insert data inBetween");
+        switch (scanner.nextInt()) {
+            case 1:
+                Operations.addDataAtStart();
+                break;
+            case 2:
+                Operations.addDataAtEnd();
+                break;
+            case 3:
+                Operations.insertInBetween();
+                break;
+        }
+
+    }
+
 }
